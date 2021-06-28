a8cc5d3544a2161c6c83dbf55d0f293180eb6e54
Diego Villacis <diegovillacis101@gmail.com>
2021-06-27 20:17:20 -0500

    Adds Brave browser in debian

--- prepare-debian.sh
+++ prepare-debian.sh
@@ -36,6 +36,12 @@ sudo apt install \
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo apt install ./google-chrome-stable_current_amd64.deb
 
+# Brave Browser
+sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+sudo apt update
+sudo apt install brave-browser
+
 # VScode
 # https://code.visualstudio.com/download
 wget "https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64" vscode_installer.deb
