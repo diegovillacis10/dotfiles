c655941dae16295f40fe924107e25ceb21641f84
Diego Villacis <diegovillacis101@gmail.com>
2024-03-19 21:06:40 -0500

    changes in config

--- setup-debian.sh
+++ setup-debian.sh
@@ -48,6 +48,18 @@ curl -sS https://starship.rs/install.sh | sh
 # Install asdf
 git clone https://github.com/asdf-vm/asdf.git "${HOME}"/.asdf
 
+# Install fzf
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install
+
+# Install eza
+sudo mkdir -p /etc/apt/keyrings
+wget -qO- https://raw.githubusercontent.com/eza-community/eza/main/deb.asc | sudo gpg --dearmor -o /etc/apt/keyrings/gierens.gpg
+echo "deb [signed-by=/etc/apt/keyrings/gierens.gpg] http://deb.gierens.de stable main" | sudo tee /etc/apt/sources.list.d/gierens.list
+sudo chmod 644 /etc/apt/keyrings/gierens.gpg /etc/apt/sources.list.d/gierens.list
+sudo apt update
+sudo apt install -y eza
+
 ## INSTALL GUIs
 
 # Google Chrome
