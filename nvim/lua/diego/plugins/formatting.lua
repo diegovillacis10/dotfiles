57da56a67e8ba9fe00c36c1f770a322512945fe1
Diego Villacis <diegovillacis101@gmail.com>
2024-03-23 19:39:33 -0500

    update nvim config for formatting

--- nvim/lua/diego/plugins/formatting.lua
+++ nvim/lua/diego/plugins/formatting.lua
@@ -7,35 +7,90 @@ return {
 
     conform.setup({
       formatters_by_ft = {
-        javascript = { "prettier" },
-        typescript = { "prettier" },
+        javascript = { { "prettierd", "prettier" } },
+        typescript = { { "prettierd", "prettier" } },
         javascriptreact = { "prettier" },
         typescriptreact = { "prettier" },
-        svelte = { "prettier" },
-        css = { "prettier" },
-        scss = { "stylelint", "prettier" },
-        sass = { "prettier" },
-        html = { "prettier" },
-        json = { "prettier" },
-        yaml = { "prettier" },
-        markdown = { "prettier" },
-        graphql = { "prettier" },
+        handlebars = { { "prettierd", "prettier" } },
+        css = { { "stylelint", "prettierd" } },
+        scss = { { "prettierd" } },
+        sass = { { "stylelint", "prettierd" } },
+        html = { { "prettierd", "prettier" } },
+        json = { { "prettierd", "prettier" } },
+        yaml = { { "prettierd", "prettier" } },
+        markdown = { { "prettierd", "prettier" } },
+        graphql = { { "prettierd", "prettier" } },
         lua = { "stylua" },
         ruby = { "rubocop" },
       },
-      -- format_on_save = {
-      --   lsp_fallback = true,
-      --   async = false,
-      --   timeout_ms = 1000,
-      -- },
+      format_on_save = function(bufnr)
+        -- Disable autoformat on certain filetypes
+        local ignore_filetypes = { "sql", "java", "scss" }
+        if vim.tbl_contains(ignore_filetypes, vim.bo[bufnr].filetype) then
+          return
+        end
+
+        -- Disable with a global or buffer-local variable
+        if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+          return
+        end
+
+        -- Disable autoformat for files in a certain path
+        local bufname = vim.api.nvim_buf_get_name(bufnr)
+        if bufname:match("/node_modules/") then
+          return
+        end
+
+        return {
+          lsp_fallback = true,
+          async = true,
+        }
+      end,
+    })
+
+    vim.api.nvim_create_user_command("Format", function(args)
+      local range = nil
+      if args.count ~= -1 then
+        local end_line = vim.api.nvim_buf_get_lines(0, args.line2 - 1, args.line2, true)[1]
+        range = {
+          start = { args.line1, 0 },
+          ["end"] = { args.line2, end_line:len() },
+        }
+      end
+      conform.format({ async = true, lsp_fallback = true, range = range })
+    end, { range = true })
+
+    vim.api.nvim_create_user_command("FormatDisable", function(args)
+      if args.bang then
+        -- FormatDisable! will disable formatting just for this buffer
+        vim.b.disable_autoformat = true
+      else
+        vim.g.disable_autoformat = true
+      end
+    end, {
+      desc = "Disable autoformat-on-save",
+      bang = true,
+    })
+
+    vim.api.nvim_create_user_command("FormatEnable", function()
+      vim.b.disable_autoformat = false
+      vim.g.disable_autoformat = false
+    end, {
+      desc = "Re-enable autoformat-on-save",
     })
 
     vim.keymap.set({ "n", "v" }, "<leader>mp", function()
       conform.format({
         lsp_fallback = true,
-        async = false,
-        timeout_ms = 1000,
+        async = true,
       })
     end, { desc = "Format file or range (in visual mode)" })
+
+    vim.keymap.set(
+      { "n", "v" },
+      "<leader>ms",
+      "<cmd>!npx stylelint '" .. vim.fn.expand("%") .. "' --fix<cr>",
+      { desc = "Format file or range (in visual mode)" }
+    )
   end,
 }
