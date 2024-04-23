8d655abd7ac9281126702e79e098901c3b437bb8
Diego Villacis <diegovillacis101@gmail.com>
2024-04-22 19:16:28 -0500

    clean up and new tmux theme

--- wezterm.lua
+++ wezterm.lua
@@ -1,10 +1,14 @@
 local wezterm = require("wezterm")
 
-local config = wezterm.config_builder()
+local config = {}
+
+if wezterm.config_builder then
+  config = wezterm.config_builder()
+end
 
 config.color_scheme = "Tokyo Night"
 
-config.font = wezterm.font("Hack Nerd Font")
+config.font = wezterm.font("Hack Nerd Font", { weight = "Bold" })
 config.font_size = 14.0
 
 config.hide_tab_bar_if_only_one_tab = true
